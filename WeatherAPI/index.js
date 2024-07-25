fb2671cf2e1cd9ff52058bc8d7053ee49fb2f3d7
conradMare <158014474+conradMare@users.noreply.github.com>
2024-07-25 07:29:32 +0200

    2024/07/25(am)
    Weather API Application - displayError && weatherForm.addEventListener functions

---
+++ WeatherAPI/index.js
@@ -0,0 +1,49 @@
+const weatherForm = document.querySelector(".weatherForm");
+const cityInput = document.querySelector(".cityInput");
+const card = document.querySelector(".card");
+
+const apiKey = process.env.API_KEY;
+
+weatherForm.addEventListener("submit", async event => {
+
+    event.preventDefault();
+
+    const city = cityInput.value;
+
+    if(city) {
+        try {
+            const weatherData = await getWeatherData(city);
+            displayWeatherInfo(weatherData);
+        }
+        catch(error) {
+            console.error(error);
+            displayError(error);
+        }
+    }
+    else {
+        displayError("Please enter a city")
+    }
+});
+
+async function getWeatherData(city) {
+
+}
+
+function displayWeatherInfo(data) {
+
+}
+
+function getWeatherEmoji(weatherId) {
+
+}
+
+function displayError(message) {
+
+    const errorDisplay = document.createElement("p");
+    errorDisplay.textContent = message;
+    errorDisplay.classList.add("errorDisplay");
+
+    card.textContent = "";
+    card.style.display = "flex";
+    card.appendChild(errorDisplay);
+}
